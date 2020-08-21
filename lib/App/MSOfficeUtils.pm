156d9fc0e11ca821d315f44720f8a7900d121252
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-08-21 15:54:19 +0700

    Initial commit

---
+++ lib/App/MSOfficeUtils.pm
@@ -0,0 +1,26 @@
+package App::MSOfficeUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to Microsoft Office
+
+=head1 DESCRIPTION
+
+B<CURRENTLY PLACEHOLDER ONLY>.
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::LibreOfficeUtils>
+
+L<App::OfficeUtils>
+
+=cut
